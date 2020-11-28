81f2798bc511916136639b0f83a33c9c34cc62ea
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2020-11-28 12:29:25 -0500

    Create onClick function to convert celsius to fahrenheit and add the event listener to the fahrenheit button

--- app/src/main/java/com/rafbatista/fahrenheitconverter/MainActivity.kt
+++ app/src/main/java/com/rafbatista/fahrenheitconverter/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun convertToCelsius(view: View) {
-        val userInput = binding.editTextFahrenheitInput.text
+        val userInput = binding.editTextTempInput.text
         val fahrenheitTemp =  userInput.toString().toFloat()
         val celsiusTemp = ( (fahrenheitTemp  -  32.0  ) *  5.0) / 9.0
         val roundedCelsiusTemp = String.format("%.2f", celsiusTemp)
@@ -33,4 +33,20 @@ class MainActivity : AppCompatActivity() {
         binding.convertedTemp.setText(roundedCelsiusTemp)
 
     }
+
+    fun convertToFahrenheit(view: View) {
+        val userInput = binding.editTextTempInput.text
+        val celciusTemp =  userInput.toString().toFloat()
+        val fahrenheitTemp = ( (celciusTemp  * 9.0 / 5.0) ) + 32.0
+        val roundedFahrenheitTemp = String.format("%.2f", fahrenheitTemp)
+        Log.i("Converted Temp" , roundedFahrenheitTemp)
+
+        val toast = Toast.makeText(this, "Success", Toast.LENGTH_LONG)
+        toast.setGravity(Gravity.TOP, 0, 225)
+        toast.show()
+
+        binding.convertedTemp.setText(roundedFahrenheitTemp)
+
+    }
+
 }

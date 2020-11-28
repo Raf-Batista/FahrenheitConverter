a1499a12c00a485e2fb78e0647e7666cf8878bf6
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2020-11-27 23:12:22 -0500

    Add onClick function t0 convert a user's typed in fahrenheit temp and converts it to celsius

--- app/src/main/java/com/rafbatista/fahrenheitconverter/MainActivity.kt
+++ app/src/main/java/com/rafbatista/fahrenheitconverter/MainActivity.kt
@@ -3,6 +3,10 @@ package com.rafbatista.fahrenheitconverter
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Gravity
+import android.view.View
+import android.widget.Toast
 import com.rafbatista.fahrenheitconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +17,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+    }
+
+    fun handleClick(view: View) {
+        val userInput = binding.editTextFahrenheitInput.text
+        val fahrenheitTemp =  userInput.toString().toFloat()
+        val celsiusTemp = ( (fahrenheitTemp  -  32.0  ) *  5.0) / 9.0
+        val roundedCelsiusTemp = String.format("%.2f", celsiusTemp)
+        Log.i("Converted Temp" , roundedCelsiusTemp)
+
+        val toast = Toast.makeText(this, "Success", Toast.LENGTH_LONG)
+        toast.setGravity(Gravity.TOP, 0, 225)
+        toast.show()
+
+        binding.convertedTemp.setText(roundedCelsiusTemp)
+
     }
 }

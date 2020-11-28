1af8594b7fbe737cae903b268bbaa6525f0c7b6b
AudioInjected <43612233+AudioInjected@users.noreply.github.com>
2020-11-27 22:11:00 -0500

    Set up view binding

--- app/src/main/java/com/rafbatista/fahrenheitconverter/MainActivity.kt
+++ app/src/main/java/com/rafbatista/fahrenheitconverter/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.rafbatista.fahrenheitconverter
 
+import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.rafbatista.fahrenheitconverter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
